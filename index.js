40ae28633e2ae06864a290289b8541ee64160dc5
foobert <0x002a@gmail.com>
2017-02-06 21:30:21 +0100

    webify

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+/*
 var fs = require('fs');
 var convnetjs = require('convnetjs');
 var cnnutil = require('convnetjs/build/util');
 global.convnetjs = convnetjs;
 global.cnnutil = cnnutil;
 var deepqlearn = require('convnetjs/build/deepqlearn');
+*/
 
 class Brain {
     constructor(width, height) {
@@ -13,7 +15,8 @@ class Brain {
         const network_size = num_inputs*temporal_window + num_actions*temporal_window + num_inputs;
 
         let layer_defs = [];
-        layer_defs.push({type:'input', out_sx:1, out_sy:1, out_depth:network_size});
+        layer_defs.push({type:'input', out_sx:1, out_sy:1, out_depth:num_inputs});
+        //layer_defs.push({type:'conv', sx:3, filters:8, stride:1, activation:'relu'});
         layer_defs.push({type:'fc', num_neurons: 50, activation:'relu'});
         layer_defs.push({type:'fc', num_neurons: 50, activation:'relu'});
         layer_defs.push({type:'regression', num_neurons:num_actions});
@@ -32,7 +35,7 @@ class Brain {
         opt.layer_defs = layer_defs;
         opt.tdtrainer_options = tdtrainer_options;
 
-        this.brain = new deepqlearn.Brain(num_inputs, num_actions);
+        this.brain = new deepqlearn.Brain(num_inputs, num_actions, opt);
     }
 
     load() {
@@ -46,18 +49,20 @@ class Brain {
     }
 
     play(cf) {
-        let action = this.brain.forward(cf.field);
-        let xy = cf.field.reverseIndex(action);
+        let input = cf.field.arr.map(x => x === null ? -1 : x);
+        let action = this.brain.forward(input);
+        let [x, y] = cf.field.reverseIndex(action);
         let result = cf.click(action);
         if (result === false) {
             // boom
-            this.brain.backward(-10);
+            this.brain.backward(-1);
         } else if (result === 0) {
             // punish no-ops
-            this.brain.backward(-1);
+            this.brain.backward(0);
         } else {
             this.brain.backward(1);
         }
+        return action;
     }
 
     playLoop(cf) {
@@ -140,6 +145,15 @@ class Minefield {
             const prev = this.field.updateIndex(this.field.randomIndex(), true);
             if (!prev) { mines--; }
         }
+        //this.field.updateIndex(5, true);
+        //this.field.updateIndex(8, true);
+        //this.field.updateIndex(13, true);
+        //this.field.updateIndex(20, true);
+        //this.field.updateIndex(22, true);
+        //this.field.updateIndex(50, true);
+        //this.field.updateIndex(51, true);
+        //this.field.updateIndex(59, true);
+        //this.field.updateIndex(70, true);
     }
 
     get width() { return this.field.width; }
@@ -220,20 +234,151 @@ class Countfield {
     }
 }
 
-let br = new Brain(9, 9);
-br.load();
+class Screen {
+    constructor() {
+        this.canvas = document.getElementById('minefield');
+        this.ctx = this.canvas.getContext('2d');
+    }
 
-for (var i = 0; i < 100; i++) {
-    let mf = new Minefield(9, 9, 10);
-    let cf = new Countfield(mf);
+    draw(cf) {
+        this.ctx.font = '48px sans-serif';
+        this.ctx.textBaseline = 'hanging';
+        for (var i = 0; i < cf.field.arr.length; i++) {
+            let [x, y] = cf.field.reverseIndex(i);
+            this.ctx.save();
+            this.ctx.translate(x * 50, y * 50);
 
-    console.log(br.playLoop(cf));
+
+            let v = cf.field.valueIndex(i);
+            if (v !== null) {
+                this.ctx.fillStyle = '#ddd';
+                this.ctx.fillRect(0, 0, 50, 50);
+
+                if (v !== 0) {
+                    this.ctx.fillStyle = '#000';
+                    this.ctx.fillText(v, 10, 10, 50);
+                }
+            } else {
+                this.ctx.fillStyle = '#bbb';
+                this.ctx.fillRect(0, 0, 50, 50);
+            }
+
+            this.ctx.strokeStyle = '#000';
+            this.ctx.strokeRect(0, 0, 50, 50);
+
+            this.ctx.restore();
+        }
+
+        for (var i = 0; i < cf.minefield.field.arr.length; i++) {
+            let [x, y] = cf.minefield.field.reverseIndex(i);
+            this.ctx.save();
+            this.ctx.translate(x * 50, y * 50);
+
+
+            let v = cf.minefield.field.valueIndex(i);
+            if (v) {
+                this.ctx.fillStyle = 'rgba(255, 0, 0, 0.3)';
+                this.ctx.fillRect(0, 0, 50, 50);
+            }
+            this.ctx.restore();
+        }
+    }
+
+    drawAction(cf, action) {
+        let [x, y] = cf.field.reverseIndex(action);
+        this.ctx.save();
+        this.ctx.translate(x * 50, y * 50);
+        this.ctx.fillStyle = 'rgba(0, 0, 255, 0.3)';
+        this.ctx.fillRect(0, 0, 50, 50);
+        this.ctx.restore();
+    }
 }
 
-br.save();
+var reward_graph = new cnnvis.Graph();
+let draw_stats = () => {
+    var canvas = document.getElementById("vis_canvas");
+    var ctx = canvas.getContext("2d");
+    var W = canvas.width;
+    var H = canvas.height;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var b = br.brain;
+    var netin = b.last_input_array;
+    ctx.strokeStyle = "rgb(0,0,0)";
+    //ctx.font="12px Verdana";
+    //ctx.fillText("Current state:",10,10);
+    ctx.lineWidth = 10;
+    ctx.beginPath();
+    for(var k=0,n=netin.length;k<n;k++) {
+        ctx.moveTo(10+k*12, 120);
+        ctx.lineTo(10+k*12, 120 - netin[k] * 100);
+    }
+    ctx.stroke();
+
+    //if(clock % 2 === 0) {
+        reward_graph.add(clock/200, b.average_reward_window.get_average());
+        var gcanvas = document.getElementById("graph_canvas");
+        reward_graph.drawSelf(gcanvas);
+    //}
+}
+
+let draw_net = () => {
+    var canvas = document.getElementById("net_canvas");
+    var ctx = canvas.getContext("2d");
+    var W = canvas.width;
+    var H = canvas.height;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var L = window.br.brain.value_net.layers;
+    var dx = (W - 50)/L.length;
+    var x = 10;
+    var y = 40;
+    ctx.font="12px Verdana";
+    ctx.fillStyle = "rgb(0,0,0)";
+    ctx.fillText("Value Function Approximating Neural Network:", 10, 14);
+    for(var k=0;k<L.length;k++) {
+        if(typeof(L[k].out_act)==='undefined') continue; // maybe not yet ready
+        var kw = L[k].out_act.w;
+        var n = kw.length;
+        var dy = (H-50)/n;
+        ctx.fillStyle = "rgb(0,0,0)";
+        ctx.fillText(L[k].layer_type + "(" + n + ")", x, 35);
+        for(var q=0;q<n;q++) {
+            var v = Math.floor(kw[q]*100);
+            if(v >= 0) ctx.fillStyle = "rgb(0,0," + v + ")";
+            if(v < 0) ctx.fillStyle = "rgb(" + (-v) + ",0,0)";
+            ctx.fillRect(x,y,10,10);
+            y += 12;
+            if(y>H-25) { y = 40; x += 12};
+        }
+        x += 50;
+        y = 40;
+    }
+}
+
+
+window.br = new Brain(9, 9);
+window.clock = 0;
+let sc = new Screen();
 
 let mf = new Minefield(9, 9, 10);
 let cf = new Countfield(mf);
-br.playLoop(cf);
-cf.print();
-console.log(cf.result);
+
+setInterval(() => {
+    clock++;
+    if (cf.result === null) {
+        let action = br.play(cf);
+        sc.draw(cf);
+        sc.drawAction(cf, action);
+        draw_stats();
+        draw_net();
+    } else {
+        console.log(cf.result ? 'won' : 'lost');
+        mf = new Minefield(9, 9, 10);
+        cf = new Countfield(mf);
+    }
+}, 10);
+
+document.getElementById('step').onclick = () => {
+    let action = br.play(cf);
+    sc.draw(cf);
+    sc.drawAction(cf, action);
+};
